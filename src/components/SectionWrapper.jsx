fac06f2a1bc465e16a77eb19cc6f960c98dc3f74
anjali <anjaligandhi248@gmail.com>
2022-12-13 20:31:08 +0530

    link added for app

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -13,7 +13,7 @@ const SectionWrapper = ({title, description, mockupImg, banner, showBtn}) => {
             {showBtn &&(
                <Buttonx
                 assetsUrl = {assets.expo}
-                link = " "
+                link = "https://drive.google.com/file/d/1mYowndZw5fzePPl4usgj0pjr8XmoTSkp/view?usp=share_link"
             />)}
             </div>
             <div className={` flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
